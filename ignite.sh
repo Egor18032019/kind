155990b32448645387852a956eb569f9da79c5b3
Egor <goro4@mail.ru>
2023-12-18 17:10:41 +0500

    save: local save

--- ignite.sh
+++ ignite.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 kind create cluster --config kind.yaml -n simbir
-#kubectl apply -f habr.yaml
 kubectl apply -f ns.yaml
-#kubectl apply -f postgresql.yaml
+kubectl apply -f postgresql.yaml
 kubectl apply -f ingress.yaml
-
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
